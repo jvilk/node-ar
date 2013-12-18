a48707e65112bb6d40623b9cc7fa74ed1397f0e6
John Vilk <jvilk@cs.umass.edu>
2013-12-18 00:56:07 -0500

    Project skeleton.

---
+++ node-ar.ts
@@ -0,0 +1,11 @@
+/// <reference path="bower_components/DefinitelyTyped/node/node.d.ts" />
+export class AR {
+  constructor(private data: NodeBuffer) {
+    // 
+  }
+}
+
+export class ARRecord {
+  constructor(private data: NodeBuffer) {
+  }
+}
